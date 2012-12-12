e7ac0fe8159e54f4c4603e415d6681ce615d071d
Jose Antonio Pio Gil <josetonyp@latizana.com>
2012-12-12 11:19:29 +0100

    Fixing syntax for foodcritic checker

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,12 @@ maintainer_email  "cloudteam@mdsol.com"
 license           "Apache 2.0"
 description       "Installs and configures Graylog2"
 version           "0.0.5"
-recipe            "graylog2", "Installs and configures Graylog2"
 
 # Only supporting centos
 supports "centos"
 
 # OpsCode cookbook dependencies
+depends "build-essential"
 depends "apt" # http://community.opscode.com/cookbooks/apt
 depends "apache2" # http://community.opscode.com/cookbooks/apache2
 depends "yum"

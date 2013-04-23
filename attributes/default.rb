f4d8401f52f983d5b4cedfa1b75fe5bd8e9383fe
Daniel Hahn <dha@betterplace.org>
2013-04-23 15:16:25 +0200

    Configurable database size

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,3 +22,6 @@ default.graylog2.stream_alarms_cron_minute = "*/15"
 default.graylog2.stream_alarms_email = "graylog2alarms@example.org"
 default.graylog2.stream_subscriptions_cron_minute = "*/15"
 default.graylog2.stream_subscriptions_email = "graylog2alarms@example.org"
+
+default.graylog2.max_docs_per_index = 1000000
+default.graylog2.max_indices = 10

aa575e5305bf81b97e671a554e461279ec0fcd01
Daniel Hahn <dha@betterplace.org>
2014-04-10 20:08:11 +0200

    **#(%* Merge conflicts

--- recipes/server.rb
+++ recipes/server.rb
@@ -31,7 +31,7 @@ package "nmap" # Only for testing, but useful anyway
 
 
 # Initialize Secrets
-include Opscode::OpenSSL::Password
+::Chef::Recipe.send(:include, Opscode::OpenSSL::Password)
 node.set_unless['graylog2']['password_secret'] = secure_password
 node.set_unless['graylog2']['root_password'] = secure_password
 
